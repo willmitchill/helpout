468b7295defdc1315a9530234d5b99aedd780a81
sloptoad <louiepisani@gmail.com>
2014-07-30 16:50:23 -0700

    updated routes and added forms

--- app/actions.rb
+++ app/actions.rb
@@ -20,15 +20,14 @@ end
 post '/login' do
    @user = User.find_by(
     username: params[:username],
-    password: params[:password]
-
-    )
-   if @user
+    password: params[:password])
+  if @user
     session[:user_id] = @user.id
     redirect '/projects'
-
+    @login_failed = false
   else
-    erb :signup
+    @login_failed = true
+    erb :login
   end
 end
 
@@ -45,14 +44,57 @@ post '/signup' do
     email: params[:email],
     password: params[:password]
     )
-  @user.save
-  redirect '/projects'   
+  if @user.save
+    session[:user_id] = @user.id
+    redirect '/projects'
+  else
+    erb :'signup' 
+  end  
+end
+
+post '/logout' do
+  session[:user_id] = nil
+  redirect '/'
 end
 
 get '/projects' do
   @projects = Project.all.order(:start_date)
   erb :projects
 end
+
+get '/new_project' do
+  erb :new_project
+end
+
+post '/new_project' do
+  @project = Project.new(
+    name: params[:name],
+    start_date: params[:start_date],
+    end_date: params[:end_date],
+    location: params[:location],
+    bio: params[:bio],
+    number_of_vols: params[:number_of_vols]
+    )
+  if @project.save
+    redirect '/projects'
+  else
+    erb :new_projects
+  end
+end
+
+get '/profile/:id' do
+  @user_id = User.find_by(params[:id])
+  erb :'profile/user_profile'
+end
+
+get 'project/:id' do
+  @project_id = Project.find_by(params[:id])
+  erb :'project/project_profile'
+end
+
+
+
+
 
 
 
